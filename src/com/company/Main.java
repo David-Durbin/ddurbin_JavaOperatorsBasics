507ac9e66a6dcf3d0f2d732fb54a7bb9e84e9ac6
David Durbin <David-Durbin@users.noreply.github.com>
2019-02-01 11:49:50 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // addition
+        System.out.println("35 + 7 = " + (35 + 7));
+        // subtraction
+        System.out.println("66 - 24 = " + (66 - 24));
+        // multiplication
+        System.out.println("7 * 6 = " + (7 * 6));
+        // division
+        System.out.println("84 / 2 = " + (84 / 2));
+        // modulus
+        System.out.println("99 % 4 = " + (99 % 4));
+    }
+}
